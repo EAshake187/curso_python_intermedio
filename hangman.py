a15fdfa9b62731d0915223b79ae69b1948a95c26
Edgar A. Abreu <edgaralfonzo_10@hotmail.com>
2021-06-20 18:35:45 -0400

    Modificaciones minimas

--- hangman.py
+++ hangman.py
@@ -1,15 +1,20 @@
 import random
-from os import system
+from os import SCHED_OTHER, system
 from typing import List
 
 
 with open('./files/data.txt', 'r', encoding='utf-8') as file:
     WORDS = [word.replace('\n','') for word in file]
-word = list(random.choice(WORDS).upper())
-lines = list('-'*len(word))
 VOCALS = {'Á':'A','É':'E','Í':'I','Ó':'O','Ú':'U'}
-lives = 5
-hearts = ['♥','♥','♥','♥','♥','♥']
+
+
+def random_word(): 
+    word = list(random.choice(WORDS).upper())
+    return word
+
+
+word = random_word()
+lines = list('-'*len(word))
 
 
 def vocals_check(letter):
@@ -23,39 +28,50 @@ def vocals_check(letter):
 
 
 def main():
-    global lives
     system('clear')
     print('''
 +-+-+-+- Bienvenido(a) al juego del ahorcado! +-+-+-+-
 
 Instrucciones: adivinar la palabra, letra por letra!
 ''')
-    while lines != word and lives >= 0:
-        print('\nVidas:',' '.join(hearts))
-        print('\n',' '.join(lines))
-        #print(word) #RECORDAR BORRAR
+    score = 0
+    choice = 1
+    while choice == 1:
+        word = random_word()
+        lines = list('-'*len(word))
+        lives = 5
+        hearts = ['♥','♥','♥','♥','♥','♥']
+        while lines != word and lives >= 0:
+            print(f'\nVidas: {" ".join(hearts)}\n\n{" ".join(lines)}')
+            #print(word) #Borrar el '#' para imprimir la palabra a adivinar
+            try:
+                letter = input('Ingresa una letra: ').upper()
+                assert len(letter) > 0
+                if letter.isnumeric():
+                    raise TypeError
+                if letter in word or (vocals_check(letter)):
+                    for i, l in enumerate(word):
+                        if l in VOCALS:
+                            l = VOCALS.get(l)
+                        if l == letter:
+                            lines[i] = word[i]
+                else:
+                    hearts[lives] = '♡'
+                    lives -= 1
+            except TypeError: print('\nDebes ingresar una letra!')
+            except AssertionError: print('\nDebes ingresar una letra!')
         try:
-            letter = input('Ingresa una letra: ').upper()
-            assert len(letter) > 0
-            if letter.isnumeric():
-                raise TypeError
-            if letter in word or (vocals_check(letter)):
-                for i, l in enumerate(word):
-                    if l in VOCALS:
-                        l = VOCALS.get(l)
-                    if l == letter:
-                        lines[i] = word[i]
+            if lines == word:
+                system('clear')
+                score += 1
+                print(f'\nVidas: {" ".join(hearts)}\n\nHaz adivinado la palabra {"".join(word)}!\n\nPuntaje: {score}')
+                choice = int(input('\n1. Seguir jugando\n2. Salir \nElija una opcion: '))
             else:
-                hearts[lives] = '♡'
-                lives -= 1
-        except TypeError: print('Debes ingresar una letra!\n')
-        except AssertionError: print('Debes ingresar una letra!\n')
-    if lines == word:
-        print('\nVidas:',' '.join(hearts))
-        print(f'\nHaz adivinado la palabra {"".join(word)}!')
-    else:
-        print('\nVidas:',' '.join(hearts))
-        print(f'\nHaz perdido, la palabra era {"".join(word)}')
+                system('clear')
+                score -= 1
+                print(f'\nVidas: {" ".join(hearts)}\n\nHaz perdido, la palabra era {"".join(word)}\n\nPuntaje: {score}')
+                choice = int(input('\n1. Seguir jugando\n2. Salir \nElija una opcion: '))
+        except ValueError: choice = int(input('\n1. Seguir jugando\n2. Salir \nPor favor, elija una opcion valida: '))
 
 
 if __name__ == '__main__':
